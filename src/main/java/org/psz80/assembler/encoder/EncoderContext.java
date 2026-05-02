9d3b1ac75df50030027c020e5085bb25840c9f4e
tjhexf <tjhexf@proton.me>
2026-05-02 11:24:31 -0300

    feat and refactor: added JR

--- src/main/java/org/psz80/assembler/encoder/EncoderContext.java
+++ src/main/java/org/psz80/assembler/encoder/EncoderContext.java
@@ -8,17 +8,24 @@ public class EncoderContext {
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     public final Map<String, Integer> symbols;
 
+    private int pc = 0;
+
     public EncoderContext(Map<String, Integer> symbols) {
         this.symbols = symbols;
     }
 
     public void writeByte(int b) {
         out.write(b & 0xFF);
+        pc++;
     }
 
     public void writeWord(int w) {
         writeByte(w & 0xFF);
         writeByte((w >> 8) & 0xFF);
+    }
+
+    public int getPC() { // ✅ ADD THIS
+        return pc;
     }
 
     public byte[] getBytes() {
