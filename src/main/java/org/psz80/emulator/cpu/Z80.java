4590501c20a45c247ee42da8538bbcee8eb2dd2c
ulisses888 <ulissesjr2002@gmail.com>
2026-05-03 22:39:41 -0300

    Esqueleto do Processador

--- src/main/java/org/psz80/emulator/cpu/Z80.java
+++ src/main/java/org/psz80/emulator/cpu/Z80.java
@@ -1,4 +1,41 @@
 package org.psz80.emulator.cpu;
 
+import org.psz80.emulator.memory.Memory;
+import org.psz80.emulator.cpu.Registers;
+
 public class Z80 {
+
+    private Memory memoria;
+    private Registers registradores;
+
+    public CPU(Memory memoria, Registers registradores) {
+        this.memoria = memoria;
+        this.registradores = registradores;
+    }
+
+    public void step(){
+
+        int pcAtual = registradores.getPC();
+        int opcode = memoria.lerByte(pcAtual);
+        registradores.setPC(pcAtual + 1);
+        executarInstrucao(opcode);
+    }
+
+    //Ulisses: dps implementar a logica das instruções e adicionar o restante (estao no InstructionTable)
+    public void executarInstrucao(int opcode){
+        switch (opcode) {
+            case 0x00: //NOP
+                break;
+            case 0x76: //HALT
+                //Ulisses: parar o processador
+                break;
+                case 0x
+            case 0x04: //INC
+                break;
+            case 0x05:
+                break;
+        }
+    }
+
+
 }
