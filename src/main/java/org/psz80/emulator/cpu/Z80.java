d073e3f8892441ceb34571aece8504e704f78571
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2026-05-04 21:08:55 -0300

    Esqueleto do Processador (#14)
    * Esqueleto do Processador
    
    * Esqueleto do Processador - Fix
    
    * Criado a memoria e metodos para leitura e escrita
    
    * Movendo os registradores para a pasta CPU
    
    * Esqueleto do Processador
    
    * Esqueleto do Processador - Fix

--- src/main/java/org/psz80/emulator/cpu/Z80.java
+++ src/main/java/org/psz80/emulator/cpu/Z80.java
@@ -1,4 +1,40 @@
 package org.psz80.emulator.cpu;
 
+import org.psz80.emulator.memory.Memory;
+import org.psz80.emulator.cpu.Registers;
+
 public class Z80 {
+
+    private Memory memoria;
+    private Registers registradores;
+
+    public Z80(Memory memoria, Registers registradores) {
+        this.memoria = memoria;
+        this.registradores = registradores;
+    }
+
+    public void step(){
+
+        int pcAtual = registradores.getPC();
+        int opcode = memoria.lerByte(pcAtual);
+        registradores.setPC(pcAtual + 1);
+        executarInstrucao(opcode);
+    }
+
+    //Ulisses: dps implementar a logica das instruções e adicionar o restante (estao no InstructionTable)
+    public void executarInstrucao(int opcode){
+        switch (opcode) {
+            case 0x00: //NOP
+                break;
+            case 0x76: //HALT
+                //Ulisses: parar o processador
+                break;
+            case 0x04: //INC
+                break;
+            case 0x05:
+                break;
+        }
+    }
+
+
 }
