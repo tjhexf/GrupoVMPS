ab0a8d6b7f242d206e6a350aca5cb95db14a757a
tjhexf <tjhexf@proton.me>
2026-05-05 19:25:17 -0300

    feat: add build system that embeds javafx

--- src/main/java/org/psz80/Main.java
+++ src/main/java/org/psz80/Main.java
@@ -1,10 +1,11 @@
 package org.psz80;
 
 import org.psz80.assembler.Assembler;
+import org.psz80.ui.MainApp;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        MainApp.main(args);
     }
 }
